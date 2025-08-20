4ba5d2da60e28748f1144912c2d06a09704ce027
StephanyMil <stephanymilhomem25@gmail.com>
2025-08-20 13:37:43 -0300

    ✨ Add receitas-mobile

--- src/routes/recipeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middlewares/authMiddleware');
-const { createRecipe, getRecipes } = require('../controllers/recipeController');
-
-router.post('/', auth, createRecipe);
-router.get('/', getRecipes);
-
-module.exports = router;
